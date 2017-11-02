b6b94f37aefd265b359fd865348567aa20df54ad
Brett Higgins <brett.higgins@gmail.com>
2017-11-02 10:11:56 -0400

    Successful PoC on Android
    - Removed demo code for the reat-native-audio-streaming library, since it
      doesn't support the media formats we need.
    - Added a stop button that can make testing faster.
    
    Often I'll see these messages in `adb logcat`:
    
        W/MediaPlayer-JNI: MediaPlayer finalized without being released
    
    The react-native-sound library doesn't report this error, and the log
    contains no other useful information. So, this library is probably not a
    good choice,  since it seems unreliable (and doesn't actually do
    streaming), but it at least proves that this sort of thing is possible
    cross-platform.

--- App.js
+++ App.js
@@ -3,96 +3,116 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 
 import { ReactNativeAudioStreaming } from 'react-native-audio-streaming';
 import RNSound from 'react-native-sound';
+RNSound.setCategory('Playback');
 
 import base64 from 'base-64';
 
-const PlayButton = ({ isPlaying, url, onPress }) => (
-  <Button
-    title={isPlaying ? "Pause" : "Play"}
-    onPress={() => onPress(url)}
-  />
-);
-
-export default class App extends React.Component {
+class PlayButton extends React.Component {
   constructor() {
     super();
 
-    this.togglePlayingRNAS = this.togglePlayingRNAS.bind(this);
-    this.togglePlayingRNS = this.togglePlayingRNS.bind(this);
-
-    // 😎
-    this.url = base64.decode('aHR0cHM6Ly9zMy11cy13ZXN0LTEuYW1hem9uYXdzLmNvbS9wYXRyZW9uLXBvc3RzLzEwNDI2MDM4ODgwODA3NjU3NjUwLm1wMw==');
-
     this.state = {
-      rnas: {
-        isPlaying: false,
-        isPaused: false,
-      },
-      rns: {
-        isPlaying: false,
-        isPaused: false,
-      },
+      isPlaying: false,
+      isLoading: false,
+      isPaused: false,
     };
+    this.sound = null;
+
+    this.togglePlayingRNS = this.togglePlayingRNS.bind(this);
   }
 
-  isPlaying(key) {
-    return this.state[key].isPlaying;
-  }
-
-  isPaused(key) {
-    return this.state[key].isPaused;
-  }
-
-  togglePlayingState(key) {
+  togglePlayingState() {
     this.setState({
-      [key]: {
-        isPlaying: !this.isPlaying(key),
-        isPaused: this.isPlaying(key),
-      },
+      isPlaying: !this.state.isPlaying,
+      isPaused: this.state.isPlaying,
     });
   }
 
-  togglePlayingRNAS(url) {
-    if (this.isPlaying('rnas')) {
-      ReactNativeAudioStreaming.pause();
-    } else if (this.isPaused('rnas')) {
-      ReactNativeAudioStreaming.resume();
-    } else {
-      ReactNativeAudioStreaming.play(this.url, {
-        showInIOSMediaCenter: true,
-        showInAndroidNotifications: true,
-      });
-    }
-    this.togglePlayingState('rnas');
-  }
-
-  togglePlayingRNS(url) {
-    if (this.isPlaying('rns')) {
+  togglePlayingRNS() {
+    if (this.state.isPlaying) {
       this.sound.pause();
-    } else if (this.isPaused('rns')) {
+    } else if (this.state.isPaused) {
       this.sound.play();
     } else {
-      RNSound.setCategory('Playback');
-      this.sound = new RNSound(this.url, undefined, (error) => {
+      this.setState({ isLoading: true });
+      this.sound = new RNSound(this.props.url, undefined, (error) => {
+        this.setState({ isLoading: false });
         if (error) {
           console.log('error: ' + error);
           throw error;
         }
         console.log('loaded sound');
-        this.sound.play();
+        this.sound.play(() => {
+          this.setState({ isPlaying: false });
+          this.sound.release();
+          this.sound = null;
+        });
       });
     }
-    this.togglePlayingState('rns');
+  }
+
+  stopPlaying() {
+    this.setState({
+      isPlaying: false,
+      isLoading: false,
+      isPaused: false,
+    });
+    this.sound.stop().release();
+  }
+
+  togglePlaying() {
+    const dispatch = {
+      'rnas': this.togglePlayingRNAS,
+      'rns': this.togglePlayingRNS,
+    };
+    dispatch[this.props.type]();
+    this.togglePlayingState();
+  }
+
+  getTitle() {
+    if (this.state.isLoading) {
+      return 'Loading';
+    }
+
+    return this.state.isPlaying ? 'Pause' : 'Play';
+  }
+
+  render() {
+    return (
+      <View style={{flexDirection: 'row'}}>
+        <Button
+          title={this.getTitle()}
+          onPress={() => this.togglePlaying()}
+        />
+        <Button
+          title="Stop"
+          disabled={!(this.state.isPlaying || this.state.isPaused)}
+          onPress={() => this.stopPlaying()}
+        />
+      </View>
+    );
+  }
+}
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.urls = [
+      base64.decode('aHR0cHM6Ly9zMy11cy13ZXN0LTEuYW1hem9uYXdzLmNvbS9wYXRyZW9uLXBvc3RzLzEwNDI2MDM4ODgwODA3NjU3NjUwLm1wMw=='),
+      base64.decode('aHR0cHM6Ly9zMy11cy13ZXN0LTEuYW1hem9uYXdzLmNvbS9wYXRyZW9uLXBvc3RzL3NBcWpRZ0xZejBtc01VaHpwZnBTa1BHR0FLeFVDZ2c1SU5rdGJlYUN6MndOeG4tVDJ4LTNfWm4zQWRJN2VKRGkubXAzCg=='),
+    ];
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>react-native-audio-streaming</Text>
-        <PlayButton isPlaying={this.state.rnas.isPlaying} onPress={this.togglePlayingRNAS} />
-
         <Text>react-native-sound</Text>
-        <PlayButton isPlaying={this.state.rns.isPlaying} onPress={this.togglePlayingRNS} />
+        {
+          this.urls.map(url => (
+            <PlayButton key={url} url={url} type="rns" />
+          ))
+        }
       </View>
     );
   }
